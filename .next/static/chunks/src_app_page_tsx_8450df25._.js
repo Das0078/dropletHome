27c0d71a672e74a23004f343c1463a931c9b2d26
Das0078 <dasdassd007@gmail.com>
2025-06-24 20:11:58 +0530

    yup

---
+++ .next/static/chunks/src_app_page_tsx_8450df25._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_f7a1fc67._.js",
+  "static/chunks/src_d02160d7._.js"
+],
+    source: "dynamic"
+});
